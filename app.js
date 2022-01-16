c9bf8d3c4f103cf3f120184f23a7682d08d0166a
ismailhossain1991 <ismail017456@gmail.com>
2022-01-16 18:47:38 +0600

    basic todo app file upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const todo = document.getElementById('do');
+const add = document.getElementById('add');
+const list = document.getElementById('list');
+
+
+
+add.addEventListener('click', () =>{
+ 
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.innerText = todo.value;
+    list.appendChild(li);
+
+    todo.value = '';
+});
+
+
